0e0c8fd5a33cb0d337f63720a747383db3993e3a
Ondřej Caletka <ondrej@caletka.cz>
2019-05-10 01:14:56 +0200

    Fix deploy

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -2,13 +2,13 @@
 
 git clone --branch=gh-pages https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages 2>&1 > /dev/null
 pushd gh-pages
-export OUTDIR="$(date +%Y-%m-%d)/"
+export OUTDIR="$(date +%Y-%m-%d)"
 mkdir -p "$OUTDIR"
-mv ../domains-secured.txt ../domains-secured.csv ../domains-stats.csv "$OUTDIR"
+mv ../domains-secured.txt ../domains-secured.csv ../domains-stats.csv "$OUTDIR"/
 # Commit and push latest version
 git add "$OUTDIR"
 git config user.name  "Travis"
 git config user.email "travis@travis-ci.org"
-git commit -m "Updated stats."
+git commit -m "Updated $OUTDIR stats"
 git push -q origin gh-pages 2>&1 > /dev/null
 popd
