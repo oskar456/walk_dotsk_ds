a5c64ecb24053a0ebb27b932783165dca6c24add
Ondřej Caletka <ondrej@caletka.cz>
2019-05-10 00:57:31 +0200

    Better GH-pages deployment

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git clone --branch=gh-pages https://${GITHUB_TOKEN}@github.com/$TRAVIS_REPO_SLUG gh-pages 2>&1 > /dev/null
+pushd gh-pages
+export OUTDIR="$(date +%Y-%m-%d)/"
+mkdir -p "$OUTDIR"
+mv ../domains-secured.txt ../domains-secured.csv ../domains-stats.csv "$OUTDIR"
+# Commit and push latest version
+git add "$OUTDIR"
+git config user.name  "Travis"
+git config user.email "travis@travis-ci.org"
+git commit -m "Updated stats."
+git push -q origin gh-pages 2>&1 > /dev/null
+popd
