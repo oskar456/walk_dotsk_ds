10f4d084d85ce07c0e143b3994111eb16947166a
Ondřej Caletka <ondrej@caletka.cz>
2019-05-10 14:31:20 +0200

    Proper add current symlink

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -5,10 +5,10 @@ pushd gh-pages
 export OUTDIR="$(date +%Y-%m-%d)"
 mkdir -p "$OUTDIR"
 mv ../domains-secured.txt ../domains-secured.csv ../domains-stats.csv "$OUTDIR"/
-git rm -f current
+git rm -f current || true
 ln -s "$OUTDIR" current
 # Commit and push latest version
-git add "$OUTDIR"
+git add "$OUTDIR" current
 git config user.name  "Travis"
 git config user.email "deploy@travis-ci.org"
 git commit -m "Updated $OUTDIR stats"
